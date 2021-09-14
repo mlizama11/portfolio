d60bef3094b10f97592c15c1dd9b989dba131e4d
Mauricio Lizama <mlizamaoliger@gmail.com>
2021-09-14 15:42:20 +0200

    More updates in CSS

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,7 +3,7 @@ import Aos from "aos";
 import "aos/dist/aos.css";
 
 function ContactForm() {
-  
+
   useEffect(() => {
     Aos.init({ duration: 1000, once: "true" });
   }, []);
@@ -13,28 +13,15 @@ function ContactForm() {
       <div class="Contact-Container">
         <form data-aos="zoom-in" className="Form" action="https://formsubmit.co/mlizamaoliger@gmail.com"
           method="POST" >
-          <h2 className="Contact-Title">CONTACT</h2>
-          <label htmlFor="full-name">Full Name</label>
-          <input type="text" name="name" id="full-name" placeholder="Full name" required />
-          <label htmlFor="email">
-            Email Address
-          </label>
-          <input
-            id="email"
-            type="email"
-            name="email"
-            placeholder="Email"
-            required
-          />
-          <label htmlFor="message">
-            Message
-          </label>
-          <textarea
-            id="message"
-            name="message"
-            placeholder="Your message here..."
-            required
-          />
+          <div className="Form-Body">
+            <h2 className="Contact-Title">CONTACT</h2>
+            <label htmlFor="full-name">Full Name</label>
+            <input type="text" name="name" id="full-name" placeholder="Full name" required />
+            <label htmlFor="email">Email Address</label>
+            <input id="email" type="email" name="email" placeholder="Email" required />
+            <label htmlFor="message">Message</label>
+            <textarea id="message" name="message" placeholder="Your message here..." required />
+          </div>
           <input className="Btn-Form" type="submit" value="SEND" />
         </form>
       </div>
