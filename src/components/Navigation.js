1c6c1cfb1022efd7c15b17b4288737919e657a63
Mauricio Lizama <mlizamaoliger@gmail.com>
2021-10-19 15:27:11 +0200

    Update in navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,22 +2,22 @@ import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const Navigation = () => {
-  const [state, setState] = useState(false);
+  const [toggle, setToggle] = useState(false);
 
   return (
     <nav className="Navbar">
       <div className="Brand-Title" ><NavLink exact={true} to="/">Porfolio</NavLink></div>
-      <div onClick={() => setState(!state)} className="Toggle-Button">
+      <div onClick={() => setToggle(!toggle)} className="Toggle-Button">
         <span className="Bar"></span>
         <span className="Bar"></span>
         <span className="Bar"></span>
       </div>
       <div className={`Navbar-Links ${state ? 'active' : ''}`}>
         <ul>
-          <li><NavLink exact={true} to="/about">About</NavLink></li>
-          <li><NavLink exact={true} to="/education">Education</NavLink></li>
-          <li><NavLink exact={true} to="/projects">Projects</NavLink></li>
-          <li><NavLink exact={true} to="/contact">Contact</NavLink></li>
+          <li><NavLink onClick={() => setToggle(!state)} exact={true} to="/about">About</NavLink></li>
+          <li><NavLink onClick={() => setToggle(!state)} exact={true} to="/education">Education</NavLink></li>
+          <li><NavLink onClick={() => setToggle(!state)} exact={true} to="/projects">Projects</NavLink></li>
+          <li><NavLink onClick={() => setToggle(!state)} exact={true} to="/contact">Contact</NavLink></li>
         </ul>
       </div>
     </nav>
